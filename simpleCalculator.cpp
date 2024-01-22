6bae69095935a87cf0a2d89b9f5b0b6d66ffb13f
root <root@Ahmet>
2024-01-22 13:05:39 +0100

    */ operations

--- simpleCalculator.cpp
+++ simpleCalculator.cpp
@@ -1,18 +1,27 @@
 #include <iostream>
 using namespace std;
 
-double sum(double a, double b){
-    return a+b;
+// double sum(double a, double b){
+//     return a+b;
+// }
+
+// double subtract(double a, double b){
+//     return a-b;
+// }
+
+double multiply(double a, double b){
+    return a*b;
 }
 
-double subtract(double a, double b){
-    return a-b;
+double divide(double a, double b){
+    return a/b;
 }
+
 
 
 int main (){
-    cout << sum(3.4,5)<<endl;
-    cout << subtract(3.4,5)<<endl;
+    cout << divide(3.4,5)<<endl;
+    cout << multiply(3.4,5)<<endl;
 
 
     return 0;
